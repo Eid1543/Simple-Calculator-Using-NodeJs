e8205084b268e2e9ee4047230edd981a46aa92d1
eid <edwin_moh_1995@outlook.com>
2021-06-14 12:27:42 +0300

    second commit

---
+++ Public/myScript.js
@@ -0,0 +1,30 @@
+let numberOfButtons = document.querySelectorAll(".bt").length;
+
+for (let i = 0; i < numberOfButtons; i++) {
+
+  document.querySelectorAll(".bt")[i].addEventListener("click", function() {
+
+    let value = this.value;
+
+    displayAndClear(value);
+
+  });
+
+}
+
+
+  function displayAndClear(input){
+
+  if (input == "clear") {
+    // clear the screen 
+    cal.screen.value = null;
+  }
+  else if (input == "="){
+    
+  }
+  else {
+  // conacatentation
+  cal.screen.value = cal.screen.value + input ;
+  }
+  
+  }
